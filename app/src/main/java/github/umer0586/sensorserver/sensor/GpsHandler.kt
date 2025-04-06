2979702c66559a0ec9115d2c97b822aca2b82cde
ocar1053 <ocar8951@gmail.com>
2025-04-06 12:57:09 +0800

    change gps request frequency

--- app/src/main/java/github/umer0586/sensorserver/sensor/GpsHandler.kt
+++ app/src/main/java/github/umer0586/sensorserver/sensor/GpsHandler.kt
@@ -6,17 +6,19 @@ import android.location.Location
 import android.os.Bundle
 import android.os.Looper
 import com.google.android.gms.location.*
+import github.umer0586.sensorserver.sensor.OdometryHandler
 
 class GpsHandler(
     context: Context,
+    odometryHandler: OdometryHandler,
     private val onLocationUpdated: (Location) -> Unit // callback for invoke rosbridge message
 ) {
     private val fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(context)
 
     // gps update parameters
-    private val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
-        .setMinUpdateIntervalMillis(500)
+    private val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 66)
+        .setMinUpdateIntervalMillis(33)
         .build()
 
     // callback for location updates
@@ -24,7 +26,13 @@ class GpsHandler(
         override fun onLocationResult(locationResult: LocationResult) {
             locationResult ?: return
             for (location in locationResult.locations) {
+                val gpsSpeed = location.speed
+                val bearing = location.bearing
+                val gpsTime = location.time
+                // update odometry data
+                odometryHandler.updateGpsSpeed(gpsSpeed, bearing, gpsTime)
                 onLocationUpdated(location)
+
             }
         }
     }

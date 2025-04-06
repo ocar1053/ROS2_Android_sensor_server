3de05f915b8c3f5e89c1cb8a2aa305a5e2389333
ocar1053 <ocar8951@gmail.com>
2025-04-06 17:31:28 +0800

    remove crash report

--- app/src/main/java/github/umer0586/sensorserver/MyApplication.kt
+++ app/src/main/java/github/umer0586/sensorserver/MyApplication.kt
@@ -14,31 +14,8 @@ class MyApplication : Application()
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
 
-        // TODO: Dark theme support will be added later.
         // For now, when a device is is dark mode we still use light theme (i-e no use of themes.xml night values)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-        initAcra {
-            //core configuration:
-            buildConfigClass = BuildConfig::class.java
-            reportFormat = StringFormat.JSON
-
-            //each plugin you chose above can be configured in a block like this:
-            mailSender {
-                //required
-                mailTo = "umerfarooq.phone@gmail.com"
-                //defaults to true
-                reportAsFile = false
-                //defaults to ACRA-report.stacktrace
-                //reportFileName = "Crash.txt"
-                //defaults to "<applicationId> Crash Report"
-                subject = "SensorServer Crash report"
-                //defaults to empty
-                //body = "Crash report attached"
-            }
-
-
-        }
     }
 
 }
